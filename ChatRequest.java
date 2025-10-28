25ca6e0d7b0e423bf54aa1a4ea121f81e0759f2d
DeepikaP8424 <deepikap8424@gmail.com>
2025-10-28 13:22:02 +0530

    ChatRequest.java

---
+++ ChatRequest.java
@@ -0,0 +1,7 @@
+package Agri.AgricultureBot.dto;
+
+public class ChatRequest {
+	private String question;
+    public String getQuestion() { return question; }
+	 public void setQuestion(String question) { this.question = question; }
+}
